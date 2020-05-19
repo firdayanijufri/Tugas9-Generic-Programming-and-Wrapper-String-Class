d0e9d3bbdcac3ea8b91fb8ff558b54b91346a977
firdayanijufri <61897517+firdayanijufri@users.noreply.github.com>
2020-05-19 04:50:54 -0700

    Add files via upload

---
+++ Fungsi_pada_string_class.java
@@ -0,0 +1,15 @@
+package tgs9;
+
+/*
+Nama      : firdayani_13020180089
+Kelas     : B2
+hari/tgl  : Minggu, 10 Mei 2020
+ */
+
+public class Fungsi_pada_string_class {
+    public static void main(String args[]){
+      char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.'};
+      String helloString = new String(helloArray);  
+      System.out.println( helloString );
+   }
+}
